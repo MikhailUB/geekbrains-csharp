ce6a801168bd17618fca75336dfb13b9e8cbae12
bolotovmu@mail.ru <bolotovmu@mail.ru>
2019-02-11 23:27:08 +0300

    Lesson6

---
+++ Csharp-Level2/Lesson6/EmployeesWpfApp/Objects/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace EmployeesWpfApp
+{
+	/// <summary>
+	/// Класс Сотрудник
+	/// </summary>
+	public class Employee : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public Guid Id { get; private set; }
+
+		private string _firstName;
+		public string FirstName
+		{
+			get => _firstName;
+			set
+			{
+				_firstName = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FirstName)));
+			}
+		}
+		private string _lastName;
+		public string LastName
+		{
+			get => _lastName;
+			set
+			{
+				_lastName = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastName)));
+			}
+		}
+
+		public string FullName => $"{FirstName} {LastName}";
+
+		private Department _department;
+		public Department Department
+		{
+			get => _department;
+			set
+			{
+				_department = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Department)));
+			}
+		}
+
+
+		public Employee(Department department, string firstName = null, string lastName = null)
+		{
+			Id = Guid.NewGuid();
+			Department = department ?? throw new ArgumentNullException(nameof(department));
+			FirstName = firstName ?? "Имя";
+			LastName = lastName ?? "Фамилия";
+		}
+	}
+}

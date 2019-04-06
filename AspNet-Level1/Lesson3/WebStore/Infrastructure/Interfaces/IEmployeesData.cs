3c20c3d95a795378c0d9cd7d43ceb769c5d839c0
bolotovmu@mail.ru <bolotovmu@mail.ru>
2019-04-06 12:09:51 +0300

    Asp Lesson3

---
+++ AspNet-Level1/Lesson3/WebStore/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+	public interface IEmployeesData
+	{
+		IEnumerable<Employee> GetAll();
+
+		Employee GetById(int id);
+
+		void AddNew(Employee employee);
+
+		void Delete(int id);
+
+		void SaveChanges();
+	}
+}

08e790868396e487d29d0d028e926e24e85c8236
bolotovmu@mail.ru <bolotovmu@mail.ru>
2019-04-10 14:29:32 +0300

    Asp Lesson4

---
+++ AspNet-Level1/Lesson4/WebStore.Domain/Entities/Section.cs
@@ -0,0 +1,18 @@
+﻿using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+	/// <summary>
+	/// Секция товаров
+	/// </summary>
+	public class Section : NamedEntity, IOrderedEntity
+	{
+		public int Order { get; set; }
+
+		/// <summary>
+		/// Идентификатор родительской секции
+		/// </summary>
+		public int? ParentId { get; set; }
+	}
+}
---
+++ AspNet-Level1/Lesson4/WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Components
+{
+	public class BrandsViewComponent : ViewComponent
+	{
+		private readonly IProductData _productData;
+
+		public BrandsViewComponent(IProductData productData)
+		{
+			_productData = productData;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var brands = GetBrands();
+
+			return View(brands);
+		}
+
+		private IEnumerable<BrandViewModel> GetBrands()
+		{
+			var brands = _productData.GetBrands();
+
+			return brands.Select(b => new BrandViewModel
+			{
+				Id = b.Id,
+				Name = b.Name,
+				Order = b.Order
+			});
+		}
+	}
+}
